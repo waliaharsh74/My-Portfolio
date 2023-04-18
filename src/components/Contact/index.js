5e7268fa7c4e811983e05948cf206ee7a4de75d2
Harsh <89077517+waliaharsh74@users.noreply.github.com>
2023-04-18 23:55:46 +0530

    Contact form working properly

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -55,7 +55,12 @@ const Contact = () => {
             don't hesitate to contact me using below form either.
           </p>
           <div className="contact-form">
-            <form ref={form} onSubmit={sendEmail}>
+            <form
+              action="https://formspree.io/f/mgebregp"
+              method="POST"
+              ref={form}
+              // onSubmit={sendEmail}
+            >
               <ul>
                 <li className="half">
                   <input placeholder="Name" type="text" name="name" required />
